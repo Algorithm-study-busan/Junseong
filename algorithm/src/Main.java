4748de75351a12a710f7ad3f761958c2d7689645
JunS <bjs713@pusan.ac.kr>
2023-03-21 16:22:14 +0900

    [BOJ]1920/silver4/자료구조

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -1,9 +1,14 @@
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Main {
-    public static void main(String[] args) {
-        String a = "ASDFG";
-        String[] result = a.split("");
-        for (int i = 0; i < a.length(); i++) {
-            System.out.println(result[i].equals("A"));
+    public static void main(String[] args) throws IOException {
+        InputStream in = System.in;
+
+        int a=1;
+        if (true){
+            a = 3;
         }
+        System.out.println(a);
     }
 }
